520edf32508b4bd14d1222efc9c5e6091c541cb1
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-08-26 15:26:40 +0330

    rating

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import * as z from 'zod'
-import Image from 'next/image'
+
 import Link from 'next/link'
 import Balancer from 'react-wrap-balancer'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Github, Twitter, Facebook } from 'lucide-react'
-
+import TextareaAutosize from 'react-textarea-autosize'
 // import Logo from '@/public/logo.svg'
 
 import {
@@ -22,27 +22,64 @@ import {
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 import { Container } from './Craft'
+import { User } from '@prisma/client'
+import { createReviewSchema } from '@/lib/schemas/rating'
+import { startTransition } from 'react'
+import { usePathname } from 'next/navigation'
+import { StarRating } from './StarRating'
+import { toast } from 'sonner'
+import { createReview } from '@/lib/actions/rating'
+import { BorderBeam } from '../BorderBeam'
 
-const formSchema = z.object({
-  email: z.string().email({
-    message: 'Please enter a valid email address.',
-  }),
-})
+interface FooterProps {
+  user: (User & { image: { url: string } | null }) | null
+}
 
-export default function Footer() {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+export default function Footer({ user }: FooterProps) {
+  const path = usePathname()
+
+  const form = useForm<z.infer<typeof createReviewSchema>>({
+    resolver: zodResolver(createReviewSchema),
     defaultValues: {
-      email: '',
+      comment: '',
+      rating: 5,
     },
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  function onSubmit(data: z.infer<typeof createReviewSchema>) {
+    console.log(data)
+    const formData = new FormData()
+    formData.append('comment', data.comment)
+    formData.append('rating', String(data.rating))
+    try {
+      startTransition(() => {
+        createReview(formData, path, user?.id as string)
+          .then((res) => {
+            if (res?.errors?.comment) {
+              form.setError('comment', {
+                type: 'custom',
+                message: res?.errors.comment?.join(' و '),
+              })
+            } else if (res?.errors?.rating) {
+              form.setError('rating', {
+                type: 'custom',
+                message: res?.errors.rating?.join(' و '),
+              })
+            } else if (res?.errors?._form) {
+              toast.error(res?.errors._form?.join(' و '))
+              form.setError('root', {
+                type: 'custom',
+                message: res?.errors?._form?.join(' و '),
+              })
+            }
+            // if (res?.success) {
+            //    toast.success(toastMessage)
+            // }
+          })
+          .catch(() => toast.error('مشکلی پیش آمده.'))
+      })
+    } catch (error) {}
   }
   return (
     <footer className="border-t border-white/40">
@@ -81,32 +118,59 @@ export default function Footer() {
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(onSubmit)}
-              className="not-prose space-y-4"
+              className="not-prose space-y-12 text-center "
             >
+              <h1 className="text-center text-green-600 text-xl font-semibold sm:text-lg">
+                درج نظر و دیدگاه درباره دکتر توتونیان
+              </h1>
               <FormField
                 control={form.control}
-                name="email"
+                name="comment"
                 render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      نظر خود راجع به دکتر توتونیان را بنویسید.
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        className="md:w-96"
-                        placeholder="example@fjord.dev"
+                  <FormItem className="mx-auto">
+                    <FormControl className="h-36">
+                      <TextareaAutosize
+                        placeholder="درج دیدگاه..."
+                        className="w-full md:w-[70%] text-blue-950 resize-none appearance-none overflow-hidden bg-white/70 p-2 border rounded-lg text-xl font-bold focus:outline-none"
                         {...field}
+                        // {...rest}
                       />
                     </FormControl>
-                    {/* <FormDescription>
-                      Lorem ipsum dolor sit amet.
-                    </FormDescription> */}
+                    <FormDescription className="text-slate-200/60">
+                      دیدگاه شما در صفحه عمومی دکتر نمایش داده خواهد شد.
+                    </FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <Button className="bg-gray-500" type="submit">
-                تایید
+              <FormField
+                control={form.control}
+                name="rating"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormControl className="w-full  !text-center  ">
+                      <StarRating
+                        wrapperProps={{
+                          className:
+                            'pb-2 flex !items-center !justify-center  w-full text-center',
+                        }}
+                        value={field.value}
+                        setValue={(d) => field.onChange(d)}
+                      />
+                    </FormControl>
+                    <FormDescription className="text-slate-200/60">
+                      از یک تا پنج ستاره بدهید.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="relative border border-white/50  "
+                type="submit"
+              >
+                ارسال دیدگاه
+                <BorderBeam size={150} duration={7} delay={3} />
               </Button>
             </form>
           </Form>
---
+++ lib/actions/rating.ts
@@ -0,0 +1,81 @@
+'use server'
+
+import { auth } from '@/auth'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { prisma } from '@/lib/prisma'
+import { createReviewActionSchema } from '@/lib/schemas/rating'
+
+interface CreateReviewFormState {
+  errors: {
+    comment?: string[]
+    rating?: string[]
+    _form?: string[]
+  }
+}
+
+export async function createReview(
+  formData: FormData,
+  path: string,
+  userId: string
+): Promise<CreateReviewFormState> {
+  const result = createReviewActionSchema.safeParse({
+    comment: formData.get('comment'),
+    rating: formData.get('rating'),
+  })
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors)
+    return {
+      errors: result.error.flatten().fieldErrors,
+    }
+  }
+  const session = await auth()
+  if (!session || !session.user) {
+    redirect('/login')
+    // return {
+    //   errors: {
+    //     _form: ['برای نظر دهی ابتدا باید عضو شوید.']
+    //   },
+    // }
+  }
+
+  try {
+    const alreadyRated = await prisma.review.findFirst({
+      where: {
+        userId,
+      },
+    })
+    if (alreadyRated) {
+      return {
+        errors: {
+          _form: ['شما قبلا نظر خود را ثبت کرده‌اید!'],
+        },
+      }
+    }
+
+    const review = await prisma.review.create({
+      data: {
+        comment: result.data.comment,
+        rating: +result.data.rating,
+        userId: session.user.id,
+      },
+    })
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      }
+    } else {
+      return {
+        errors: {
+          _form: ['مشکلی پیش آمده، لطفا دوباره امتحان کنید!'],
+        },
+      }
+    }
+  }
+  revalidatePath(path)
+  redirect(path)
+}
